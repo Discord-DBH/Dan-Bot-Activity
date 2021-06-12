477acd098289db122a4fbfa3b8ef88c60b9b5fce
Discord-DBH <85806019+Discord-DBH@users.noreply.github.com>
2021-06-12 20:34:36 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const chalk = require('chalk');
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const config = require("./config.json")
+const figlet = require('figlet');
+const { Client } = require("discord.js")
+//THe Packages have been Loaded
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+//The Collection for the Commands and its Aliases
+["command", "events"].forEach(handler => {
+  require(`./handlers/${handler}`)(client);
+});
+//The Handlers have Been Loaded
+client.login(config.token)
