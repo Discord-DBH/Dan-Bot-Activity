44f2204465c22ba971173a2b18c5f5b3cd42c77b
James <85806019+James-Discord@users.noreply.github.com>
2021-07-15 08:09:31 +0100

    Update forceupdate.js

--- commands/info/forceupdate.js
+++ commands/info/forceupdate.js
@@ -4,31 +4,23 @@ const { exec } = require("child_process");
 module.exports = {
   name: "forceupdate",
   description: "forceupdate",
-  async run(message, args) {
-    exec(`git pull`, (error, stdout) => {
-      let response = error || stdout;
-      if (!error) {
-        if (response.includes("Already up to date.")) {
-          message.channel.send(
-            "Bot already up to date. No changes since last pull"
-          );
-        } else {
-          client.channels.cache
-            .get("862049598801444874")
-            .send(
-              "**[AUTOMATIC]** \nNew update on GitHub. Pulling. \n\nLogs: \n```" +
-                response +
-                "```" +
-                "\n\n\n**Restarting bot**" +
-                response +
-                "THE UPDATE WAS FORCED BY A STAFF MEMBER!"
-            );
-          setTimeout(() => {
-            process.exit();
-          }, 1000);
-        }
-      }
-    });
-  },
-};
-
+async (client, message, args) => {
+    if (message.member.roles.cache.find(r => r.id === "778237595477606440")) {
+        exec(`git pull`, (error, stdout) => {
+            let response = (error || stdout);
+            if (!error) {
+                if (response.includes("Already up to date.")) {
+                    message.channel.send('Bot already up to date. No changes since last pull')
+                } else {
+                    message.channel.send('Pulled from GitHub. Restarting bot. \n\nLogs: \n```' + response + "```")
+                    setTimeout(() => {
+                        process.exit();
+                    }, 1000)
+                }
+                ;
+            }
+        });
+    } else {
+        message.channel.send('OwO')
+    }
+}}
