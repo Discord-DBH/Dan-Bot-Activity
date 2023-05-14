9ea0af3a74b51e9f8874c801b60e7992f689c7ea
James <85806019+James-Discord@users.noreply.github.com>
2023-05-14 22:12:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
+// Packages
 const Discord = require("discord.js");
 const { Client, Collection } = require("discord.js");
-const fs = require("fs");
 const config = require("./config.json");
+const fs = require("fs");
 
+// Bot
 const client = new Client({
-  intents: ["GUILDS", "GUILD_MESSAGES"],
+  intents: ["GUILDS", "GUILD_MESSAGES"]
 });
-
 client.commands = new Collection();
 client.aliases = new Collection();
-
-const commandFiles = fs.readdirSync("./commands")
-  .filter(file => file.endsWith(".js"));
-
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
-  if (command.aliases) {
-    command.aliases.forEach(alias => {
-      client.aliases.set(alias, command.name);
-    });
-  }
-}
-
-const eventFiles = fs.readdirSync("./events")
-  .filter(file => file.endsWith(".js"));
-
-for (const file of eventFiles) {
-  const event = require(`./events/${file}`);
-  if (event.once) {
-    client.once(event.name, (...args) => event.run(...args, client));
-  } else {
-    client.on(event.name, (...args) => event.run(...args, client));
-  }
-}
-
 client.login(config.token);
+
+// Command Handler
+fs.readdirSync("./commands").forEach(dir => {
+  const commandFiles = fs.readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
+
+  for (const file of commandFiles) {
+    const command = require(`./commands/${dir}/${file}`);
+    client.commands.set(command.name, command);
+    console.log(`Loaded command: ${command.name}`);
+  }
+});
+
+// Event Handler
+fs.readdirSync("./events").forEach(file => {
+  const event = require(`./events/${file}`);
+  console.log(`Loaded event: ${event.name}`);
+  client.on(event.name, (...args) => event.run(client, ...args));
+});
