88345f59fc2076abcdad2345677a66f225f25394
James <85806019+James-Discord@users.noreply.github.com>
2023-05-14 20:48:35 +0100

    Update ss.js

--- commands/info/ss.js
+++ commands/info/ss.js
@@ -1,11 +1,16 @@
-const {MessageAttachment} = require("discord.js");
-const Discord = require('discord.js');
-module.exports = {
-name: "ss",
-run: async (client, message, args) => {
-//const arg = message.content.slice(4).trim().split(/+ /);          
-let url = args.join("")
-message.channel.send(`${message.author}(${message.author.tag})`,
-new MessageAttachment(`https://image.thum.io/get/width/1920/crop/1000/noanimate/${url}`, "Screenshot.png"))
+const { MessageAttachment } = require("discord.js");
 
-}}
+module.exports = {
+  name: "ss",
+  run: async (client, message, args) => {
+    const url = args.join("");
+    const attachment = new MessageAttachment(
+      `https://image.thum.io/get/width/1920/crop/1000/noanimate/${url}`,
+      "Screenshot.png"
+    );
+    message.reply({
+      content: `${message.author} (${message.author.tag})`,
+      files: [attachment],
+    });
+  },
+};
