fd9e687c506d613d80d0923aba41d8e52c77fdbc
James <85806019+James-Discord@users.noreply.github.com>
2023-05-14 22:09:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-// Packages
 const Discord = require("discord.js");
 const { Client, Collection } = require("discord.js");
+const fs = require("fs");
 const config = require("./config.json");
 
-// Bot
 const client = new Client({
-    intents: ["GUILDS", "GUILD_MESSAGES"]
+  intents: ["GUILDS", "GUILD_MESSAGES"],
 });
+
 client.commands = new Collection();
 client.aliases = new Collection();
-client.login(config.token);
 
-// Command Handler
-const commandFiles = require("./handlers/command");
+const commandFiles = fs.readdirSync("./commands")
+  .filter(file => file.endsWith(".js"));
+
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+  if (command.aliases) {
+    command.aliases.forEach(alias => {
+      client.aliases.set(alias, command.name);
+    });
+  }
 }
 
-// Event Handler
-const eventFiles = require("./handlers/event");
+const eventFiles = fs.readdirSync("./events")
+  .filter(file => file.endsWith(".js"));
+
 for (const file of eventFiles) {
-    const event = require(`./events/${file}`);
-    if (event.once) {
-        client.once(event.name, (...args) => event.run(...args, client));
-    } else {
-        client.on(event.name, (...args) => event.run(...args, client));
-    }
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.run(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.run(...args, client));
+  }
 }
+
+client.login(config.token);
