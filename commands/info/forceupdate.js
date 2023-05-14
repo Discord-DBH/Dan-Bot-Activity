db53deb03fcd406b7fa3278fc6cc6e48b63058b0
James <85806019+James-Discord@users.noreply.github.com>
2023-05-14 22:13:59 +0100

    Update forceupdate.js

--- commands/info/forceupdate.js
+++ commands/info/forceupdate.js
@@ -1,26 +1,26 @@
-const Discord = require("discord.js");
 const { exec } = require("child_process");
 
 module.exports = {
   name: "forceupdate",
   description: "forceupdate",
-async (client, message, args) => {
-    if (message.member.roles.cache.find(r => r.id === "778237595477606440")) {
-        exec(`git pull`, (error, stdout) => {
-            let response = (error || stdout);
-            if (!error) {
-                if (response.includes("Already up to date.")) {
-                    message.channel.send('Bot already up to date. No changes since last pull')
-                } else {
-                    message.channel.send('Pulled from GitHub. Restarting bot. \n\nLogs: \n```' + response + "```")
-                    setTimeout(() => {
-                        process.exit();
-                    }, 1000)
-                }
-                ;
-            }
-        });
+  async execute(client, interaction) {
+    const { member } = interaction;
+    if (member.roles.cache.find(r => r.id === "778237595477606440")) {
+      exec(`git pull`, (error, stdout) => {
+        let response = (error || stdout);
+        if (!error) {
+          if (response.includes("Already up to date.")) {
+            interaction.reply('Bot already up to date. No changes since last pull');
+          } else {
+            interaction.reply('Pulled from GitHub. Restarting bot. \n\nLogs: \n```' + response + "```");
+            setTimeout(() => {
+              process.exit();
+            }, 1000);
+          }
+        }
+      });
     } else {
-        message.channel.send('OwO')
+      interaction.reply('OwO');
     }
-}}
+  }
+};
