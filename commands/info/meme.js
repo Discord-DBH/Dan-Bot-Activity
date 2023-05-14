91be7cc4e40b30a84b1641a930fb98f2299e8f81
James <85806019+James-Discord@users.noreply.github.com>
2023-05-14 22:17:31 +0100

    Update meme.js

--- commands/info/meme.js
+++ commands/info/meme.js
@@ -1,29 +1,30 @@
 const Discord = require('discord.js');
-const got = require('got');
 module.exports = {
   name: "oddlysatisfying",
   run: async (client, message, args) => {
     const embed = new Discord.MessageEmbed();
-    got('https://www.reddit.com/r/memes/random/.json')
-      .then(response => {
-        const [list] = JSON.parse(response.body);
-        const [post] = list.data.children;
+    import('got').then(got => {
+      got('https://www.reddit.com/r/memes/random/.json')
+        .then(response => {
+          const [list] = JSON.parse(response.body);
+          const [post] = list.data.children;
 
-        const permalink = post.data.permalink;
-        const memeUrl = `https://reddit.com${permalink}`;
-        const memeImage = post.data.url;
-        const memeTitle = post.data.title;
-        const memeUpvotes = post.data.ups;
-        const memeNumComments = post.data.num_comments;
+          const permalink = post.data.permalink;
+          const memeUrl = `https://reddit.com${permalink}`;
+          const memeImage = post.data.url;
+          const memeTitle = post.data.title;
+          const memeUpvotes = post.data.ups;
+          const memeNumComments = post.data.num_comments;
 
-        embed.setTitle(`**Meme Title:** ${memeTitle}`);
-        embed.setURL(`${memeUrl}`);
-        embed.setColor('RANDOM');
-        embed.setImage(memeImage);
-        embed.setDescription(`**Upvotes and comments:** 👍 ${memeUpvotes} 💬 ${memeNumComments}`);
+          embed.setTitle(`**Meme Title:** ${memeTitle}`);
+          embed.setURL(`${memeUrl}`);
+          embed.setColor('RANDOM');
+          embed.setImage(memeImage);
+          embed.setDescription(`**Upvotes and comments:** 👍 ${memeUpvotes} 💬 ${memeNumComments}`);
 
-        message.channel.send(embed);
-      })
-      .catch(console.error);
+          message.channel.send(embed);
+        })
+        .catch(console.error);
+    }).catch(console.error);
   }
-}
+};
