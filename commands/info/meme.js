fa316b899594a4be0274fdef0bda3b6a7414a4fd
James <85806019+James-Discord@users.noreply.github.com>
2023-05-14 22:16:09 +0100

    Update meme.js

--- commands/info/meme.js
+++ commands/info/meme.js
@@ -1,33 +1,29 @@
-import { MessageEmbed } from 'discord.js';
-import got from 'got';
+const Discord = require('discord.js');
+const got = require('got');
+module.exports = {
+  name: "oddlysatisfying",
+  run: async (client, message, args) => {
+    const embed = new Discord.MessageEmbed();
+    got('https://www.reddit.com/r/memes/random/.json')
+      .then(response => {
+        const [list] = JSON.parse(response.body);
+        const [post] = list.data.children;
 
-export default {
-  name: 'meme',
-  description: 'Sends a random meme from the /r/memes subreddit',
-  async execute(client, interaction) {
-    const embed = new MessageEmbed();
+        const permalink = post.data.permalink;
+        const memeUrl = `https://reddit.com${permalink}`;
+        const memeImage = post.data.url;
+        const memeTitle = post.data.title;
+        const memeUpvotes = post.data.ups;
+        const memeNumComments = post.data.num_comments;
 
-    try {
-      const response = await got('https://www.reddit.com/r/memes/random/.json').json();
-      const [post] = response[0].data.children;
+        embed.setTitle(`**Meme Title:** ${memeTitle}`);
+        embed.setURL(`${memeUrl}`);
+        embed.setColor('RANDOM');
+        embed.setImage(memeImage);
+        embed.setDescription(`**Upvotes and comments:** 👍 ${memeUpvotes} 💬 ${memeNumComments}`);
 
-      const permalink = post.data.permalink;
-      const memeUrl = `https://reddit.com${permalink}`;
-      const memeImage = post.data.url;
-      const memeTitle = post.data.title;
-      const memeUpvotes = post.data.ups;
-      const memeNumComments = post.data.num_comments;
-
-      embed.setTitle(`**Meme Title:** ${memeTitle}`);
-      embed.setURL(`${memeUrl}`);
-      embed.setColor('RANDOM');
-      embed.setImage(memeImage);
-      embed.setDescription(`**Upvotes and comments:** 👍 ${memeUpvotes} 💬 ${memeNumComments}`);
-
-      await interaction.reply({ embeds: [embed] });
-    } catch (err) {
-      console.error(err);
-      await interaction.reply({ content: 'An error occurred while fetching the meme.' });
-    }
-  },
-};
+        message.channel.send(embed);
+      })
+      .catch(console.error);
+  }
+}
